9eb6b0cbe796c032cde83992e0c3e4827ca4099c
EtharSamaha <etharsamaha@gmail.com>
2020-01-04 15:31:29 +0200

    uploading the project

---
+++ src/testdatabase/DBHelper.java
@@ -0,0 +1,23 @@
+package testdatabase;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author Imad
+ */
+public class DBHelper {
+
+    public static Connection connectDB() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/students", "root", "root");
+            return conn;
+        } catch (ClassNotFoundException ex) {
+            System.err.println("Class Not Found");
+        } catch (SQLException ex) {
+            System.err.println(ex);
+        }
+        return null;
+    }
+}
